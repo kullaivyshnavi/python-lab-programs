03c1e8d98bf12c155cd9a2f07a08b8044999851e
vyshu <kullaivyshnavi@gmail.com>
2026-09-23 22:04:47 +0530

    add new program

---
+++ python 1g.py
@@ -0,0 +1,21 @@
+num = 25
+print("Integer Value:", num)
+print("Converted to Float:", float(num))
+price = 45.75
+print("\nFloat Value:", price)
+print("Converted to Integer:", int(price))
+age = 20
+print("\nInteger Value:", age)
+print("Converted to String:", str(age))
+number = "150"
+print("\nString Value:", number)
+print("Converted to Integer:", int(number))
+value = 1
+print("\nInteger Value:", value)
+print("Converted to Boolean:", bool(value))
+print("\nData Types After Conversion")
+print("float(num):", type(float(num)))
+print("int(price):", type(int(price)))
+print("str(age):", type(str(age)))
+print("int(number):", type(int(number)))
+print("bool(value):", type(bool(value)))
