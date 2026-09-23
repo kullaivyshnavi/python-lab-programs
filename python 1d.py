ad543e2787a1864828f34f78eb8b81fee3601393
vyshu <kullaivyshnavi@gmail.com>
2026-09-23 21:43:26 +0530

    add a new program

---
+++ python 1d.py
@@ -0,0 +1,3 @@
+message=input("enter your message:")
+print("\n user-defined message:")
+print(message)
---
+++ python 1e.py
@@ -0,0 +1,18 @@
+a = 100
+b = 25.75
+c = "Python"
+d = True
+e = 3 + 4j
+f = [10, 20, 30]
+g = (1, 2, 3)
+h = {"Name": "John", "Age": 20}
+
+# Display data types
+print("Value:", a, "Type:", type(a))
+print("Value:", b, "Type:", type(b))
+print("Value:", c, "Type:", type(c))
+print("Value:", d, "Type:", type(d))
+print("Value:", e, "Type:", type(e))
+print("Value:", f, "Type:", type(f))
+print("Value:", g, "Type:", type(g))
+print("Value:", h, "Type:", type(h))
