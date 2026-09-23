a944249302fce27f0b486ebcd3e7fe758360c5e2
vyshu <kullaivyshnavi@gmail.com>
2026-09-23 19:51:36 +0530

    add new program

---
+++ python 1c.py
@@ -0,0 +1,8 @@
+c1 = complex(input("Enter first complex number (a+bj): "))
+c2 = complex(input("Enter second complex number (a+bj): "))
+addition = c1 + c2
+multiplication = c1 * c2
+print("\nFirst Complex Number:", c1)
+print("Second Complex Number:", c2)
+print("Addition:", addition)
+print("Multiplication:", multiplication)
